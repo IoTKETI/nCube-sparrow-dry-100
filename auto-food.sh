c628a9d21d65311fe0269ba80f4e6f04e7bf1dca
IoTKETI <iot.keti@gmail.com>
2020-05-26 18:13:05 +0900

    edit

--- auto-food.sh
+++ auto-food.sh
@@ -1,8 +1,8 @@
   
 #!/usr/bin/sh
 
-sudo chmod 777 /home/pi/nCube-sparrow-dry-sx1509/
-cd /home/pi/nCube-sparrow-dry-sx1509
+sudo chmod 777 /home/pi/nCube-sparrow-dry-100/
+cd /home/pi/nCube-sparrow-dry-100
 sudo chmod 777 *
 python3 start.py
 git stash
@@ -10,11 +10,11 @@ git pull
 sleep 5
 git stash pop
 python3 exec_print.py &
-sleep 1
+sleep 2
 python3 exec_buzzer.py &
 sleep 2
 python3 exec_res.py &
-sleep 1
+sleep 2
 python3 exec_res2.py &
 sleep 2
 pm2 start thyme.js
